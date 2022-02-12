102309e8d8203983014a431da4a9ac61f251632b
Calente Cardwell <nxmxgoldxx@live.com>
2022-02-12 14:30:14 -0800

    imported,  arrow function with children,  export

--- src/NameTag.js
+++ src/NameTag.js
@@ -1,4 +1,6 @@
 // import the react library to write JSX
+import React from "react";
+
 
 /* write an arrow function called "NameTag" that returns JSX
 The JSX returned should consist of:
@@ -8,5 +10,15 @@ The JSX returned should consist of:
     - a h2 element with className attribute of "name" and the inner text of your name
 */
 
+const NameTag = () => (
+  <div className="name-tag">
+    <h3 className="title">HELLO</h3>
+    <p className="subtitle">This is brought to you by</p>
+    <h2 className="name">Designs By Nome</h2>
+  </div>
+);
+
+
+export default NameTag;
 /* export our functional component definition to be 
 imported then rendered within another module */
