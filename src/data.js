dbbe8f5195cd467d2e9f17a023ecb93ff1c01163
Calente Cardwell <nxmxgoldxx@live.com>
2022-02-16 14:22:45 -0800

    Building a data model, name tag generator

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const names = [  
+    { person: "Doug", id: "001" },
+    { person: "Mom", id: "002" },
+    { person: "Meredith", id: "003" },
+    { person: "Kwan", id: "004" },
+    { person: "KC", id: "005" },
+    { person: "Caleb", id: "006" },
+    { person: "Courtney", id: "007" },
+    { person: "Kayla", id: "008" },
+    { person: "Iyana", id: "009" },
+];
